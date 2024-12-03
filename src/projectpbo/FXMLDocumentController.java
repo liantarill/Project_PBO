3bd9ec4fe23544653173a7d44419a1c03e93262e
liantarill <yusufherliananantaril@gmail.com>
2024-12-04 01:09:43 +0700

    edit enemy movement

--- src/projectpbo/FXMLDocumentController.java
+++ src/projectpbo/FXMLDocumentController.java
@@ -5,12 +5,17 @@
 package projectpbo;
 
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
+import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
@@ -23,22 +28,12 @@ import javafx.util.Duration;
 public class FXMLDocumentController implements Initializable {
 
     @FXML
-    private AnchorPane scene;
-    @FXML
-    private ImageView Hero;
-    @FXML
-    private ImageView enemy1;
-    @FXML
-    private ImageView enemy2;
-    @FXML
-    private ImageView enemy3;
-    @FXML
-    private ImageView enemy4;
-    @FXML
-    private ImageView enemy5;
-    @FXML
-    private ImageView enemy6;
+    private AnchorPane scene; 
     
+    @FXML
+    private ImageView Hero; 
+
+    private final Random random = new Random();
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -47,51 +42,86 @@ public class FXMLDocumentController implements Initializable {
         
         scene.requestFocus();
         scene.setOnMouseClicked(event -> scene.requestFocus());
-        
-        System.out.println("Scene Width: " + scene.getWidth());
-        System.out.println("Scene Height: " + scene.getHeight());
-        
-        createEnemyMovement(enemy1);
-        createEnemyMovement(enemy2);
-        createEnemyMovement(enemy3);
-        createEnemyMovement(enemy4);
-        createEnemyMovement(enemy5);
-        createEnemyMovement(enemy6);
-    }    
-//contoh
+
+        startEnemySpawner();
+    }
+
     @FXML
     private void Movement(KeyEvent event) {
-         switch(event.getCode()){
+        switch (event.getCode()) {
             case UP:
-                if(Hero.getLayoutY() > 0){
-                    Hero.setLayoutY(Hero.getLayoutY()-10);
+                if (Hero.getLayoutY() > 0) {
+                    Hero.setLayoutY(Hero.getLayoutY() - 10);
                 }
                 break;
             case DOWN:
-                if(Hero.getLayoutY() < 511){
-                    Hero.setLayoutY(Hero.getLayoutY()+10);
+                if (Hero.getLayoutY() < scene.getHeight() - Hero.getFitHeight()) {
+                    Hero.setLayoutY(Hero.getLayoutY() + 10);
                 }
                 break;
             case RIGHT:
-                if(Hero.getLayoutX() < 700){
-                    Hero.setLayoutX(Hero.getLayoutX()+10);
+                if (Hero.getLayoutX() < scene.getWidth() - Hero.getFitWidth()) {
+                    Hero.setLayoutX(Hero.getLayoutX() + 10);
                 }
                 break;
             case LEFT:
-                if(Hero.getLayoutX() > 0){
-                    Hero.setLayoutX(Hero.getLayoutX()-10);
+                if (Hero.getLayoutX() > 0) {
+                    Hero.setLayoutX(Hero.getLayoutX() - 10);
                 }
                 break;
         }
     }
-    
+
+    private void startEnemySpawner() {
+        Timeline spawner = new Timeline(new KeyFrame(Duration.seconds(2), event -> spawnEnemy()));
+        spawner.setCycleCount(Timeline.INDEFINITE);
+        spawner.play();
+    }
+
+    private void spawnEnemy() {
+        // Paths to enemy images
+        String[] enemyImages = {
+        getClass().getResource("img/enemy1.png").toExternalForm(),
+        getClass().getResource("img/enemy2.png").toExternalForm(),
+        getClass().getResource("img/enemy3.png").toExternalForm(),
+        getClass().getResource("img/enemy4.png").toExternalForm(),
+        getClass().getResource("img/enemy5.png").toExternalForm(),
+        getClass().getResource("img/enemy6.png").toExternalForm()
+        };
+
+
+        int randomIndex = random.nextInt(enemyImages.length);
+        String selectedImagePath = enemyImages[randomIndex];
+
+        double randomX = random.nextDouble() * (scene.getWidth() - 50); // Adjust for enemy size
+        double startY = -50; 
+
+        ImageView enemy = new ImageView(new Image(selectedImagePath));
+        enemy.setFitWidth(50); 
+        enemy.setFitHeight(50); 
+        enemy.setLayoutX(randomX);
+        enemy.setLayoutY(startY);
+
+        scene.getChildren().add(enemy);
+
+        createEnemyMovement(enemy);
+    }
+
     private void createEnemyMovement(ImageView enemy) {
-        TranslateTransition transition = new TranslateTransition();
-        transition.setNode(enemy);
-        transition.setByX(500);
-        transition.setDuration(Duration.seconds(5));
-        transition.setCycleCount(TranslateTransition.INDEFINITE);
-        transition.setAutoReverse(true);
-        transition.play();
+        double speed = 2.0; 
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                enemy.setLayoutY(enemy.getLayoutY() + speed);
+
+                if (enemy.getLayoutY() > scene.getHeight()) {
+                    scene.getChildren().remove(enemy);
+                    stop();
+                }
+            }
+        };
+
+        timer.start(); 
     }
 }
